e7519a58abe624e3a737f7b1cb467443da7e9a96
Disha <disha.wind@gmail.com>
2025-09-01 16:08:18 +0300

    Add porter stemmer template class

---
+++ porter_stemmer.py
@@ -0,0 +1,24 @@
+class PorterStemmer:
+
+    @staticmethod
+    def stem(word: str) -> str:
+        pass
+
+    @staticmethod
+    def __step1(word: str) -> str:
+        """
+        В последовательном порядке пытаемся удалить все виды окончаний.
+        """
+        pass
+
+    @staticmethod
+    def __step2(word: str) -> str:
+        pass
+
+    @staticmethod
+    def __step3(word: str) -> str:
+        pass
+
+    @staticmethod
+    def __step4(word: str) -> str:
+        pass
