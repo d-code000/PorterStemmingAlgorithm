da3007cb9f4ed104b94cd549110aba6f0f4e6445
Disha <disha.wind@gmail.com>
2025-09-03 22:04:25 +0300

    Add new test data

---
+++ tests/comparative/conftest.py
@@ -0,0 +1,9 @@
+import pytest
+
+
+@pytest.fixture(scope='function', params=['10', '10000', '1000000'])
+def words(request) -> list[str]:
+    filename = f'data/russian-words-{request.param}.txt'
+    with open(filename, 'r', encoding='utf-8') as file:
+        return file.read().split()
+
--- tests/conftest.py
+++ tests/conftest.py
@@ -6,9 +6,3 @@ from porter_stemmer import PorterStemmer
 @pytest.fixture(scope='session')
 def stemmer() -> PorterStemmer:
     return PorterStemmer()
-
-
-@pytest.fixture(scope='function')
-def words() -> list[str]:
-    with open('data/russian_words_10.txt', 'r', encoding='utf-8') as file:
-        return file.read().split()
