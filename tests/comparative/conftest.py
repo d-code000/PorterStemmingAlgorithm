340bfab46df2602d2ae6135304b7b07c7542e003
Disha <disha.wind@gmail.com>
2025-09-04 12:09:49 +0300

    Clean test data

--- tests/comparative/conftest.py
+++ tests/comparative/conftest.py
@@ -1,9 +1,9 @@
 import pytest
 
 
-@pytest.fixture(scope='function', params=['10', '10000', '1000000'])
+@pytest.fixture(scope='function', params=['10', '2000'])
 def words(request) -> list[str]:
     filename = f'data/russian-words-{request.param}.txt'
     with open(filename, 'r', encoding='utf-8') as file:
-        return file.read().split()
+        return file.read().split('\n')
 
