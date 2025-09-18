7c6b33e088e451cbff84c4557c603fea09ee68c9
Disha <disha.wind@gmail.com>
2025-09-18 18:50:10 +0300

    update test data path

--- tests/comparative/conftest.py
+++ tests/comparative/conftest.py
@@ -3,7 +3,7 @@ import pytest
 
 @pytest.fixture(scope='function', params=['10', '2000'])
 def words(request) -> list[str]:
-    filename = f'data/russian-words-{request.param}.txt'
+    filename = f'tests/data/russian-words-{request.param}.txt'
     with open(filename, 'r', encoding='utf-8') as file:
         return file.read().split('\n')
 
